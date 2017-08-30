6e8ab60ec8a4d13a7dce26d93d6dc34adc75839c
Ihor Furseyev <zebra@atwix.com>
2017-08-30 19:55:49 +0300

    Adding actual logic to show last 5 visited pages

--- view/frontend/web/js/history.js
+++ view/frontend/web/js/history.js
@@ -7,6 +7,10 @@ define(
     ],
     function (Component, $, ko, _) {
         return Component.extend({
+            sessionKey : 'last_visited_pages',
+
+            numbserOfPages : 5,
+
             historyItems: ko.observable([]),
 
             initialize: function() {
@@ -16,7 +20,25 @@ define(
             },
 
             initHistoryItems: function() {
-                this.historyItems(["Hello", "World"]);
+                var thisUrl = document.URL;
+                var thisTitle = document.title;
+                var lastPages = $.parseJSON(sessionStorage.getItem(this.sessionKey));
+                var currentPage = {title : thisTitle, url : thisUrl};
+                if (!lastPages) {
+                    lastPages = [];
+                }
+                //TODO check for duplicated URLs
+                lastPages.push(currentPage);
+                //Remove the exceeding item from the pages stack
+                if (lastPages.length > this.numbserOfPages + 1) {
+                    lastPages.shift();
+                }
+                //Set pages to the session
+                sessionStorage.setItem(this.sessionKey, JSON.stringify(lastPages));
+                //Remove the current page from the pages array before rendering
+                lastPages.pop();
+
+                this.historyItems(lastPages);
             }
         });
     }
